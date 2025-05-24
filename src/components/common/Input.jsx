1b709e5226f61d28b1e71ff03cbbea73ab1c49eb
eched1 <echezonadike1@gmail.com>
2025-05-24 17:33:28 -0500

    fix: clean footer and implement input component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 
-const Input = ({ label, id }) => {
-  // component code
-};
+const Input = ({ label, id, className = '', ...props }) => (
+  <div className="flex flex-col">
+    <label htmlFor={id} className="mb-1">
+      {label}
+    </label>
+    <input id={id} className={`border p-2 ${className}`} {...props} />
+  </div>
+);
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
+
+Input.defaultProps = {
+  className: '',
+};
+
+export default Input;
